dd9ab59c2d5a19439aec847dfd67f64e2f5a76a3
Sabir Hassan <sabirbk06@gmail.com>
2022-10-23 19:27:16 +0500

    covert keypad to qwerty

--- src/components/Keypad.tsx
+++ src/components/Keypad.tsx
@@ -1,5 +1,8 @@
 import React from "react"
-import { Letters } from "../data"
+
+const qwerty = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"]
+const asd = ["a", "s", "d", "f", "g", "h", "j", "k", "l"]
+const zxc = ["z", "x", "c", "v", "b", "n", "m"]
 
 type KeypadProps = {
   usedKeys: { [key: string]: string }
@@ -8,14 +11,36 @@ type KeypadProps = {
 export default function Keypad({ usedKeys }: KeypadProps) {
   return (
     <div className="keypad">
-      {Letters.map((letter) => {
-        const color = usedKeys[letter]
-        return (
-          <div key={letter} className={color}>
-            {letter}
-          </div>
-        )
-      })}
+      <div className="row">
+        {qwerty.map((letter) => {
+          const color = usedKeys[letter]
+          return (
+            <div key={letter} className={`item ${color}`}>
+              {letter}
+            </div>
+          )
+        })}
+      </div>
+      <div className="row">
+        {asd.map((letter) => {
+          const color = usedKeys[letter]
+          return (
+            <div key={letter} className={`item ${color}`}>
+              {letter}
+            </div>
+          )
+        })}
+      </div>
+      <div className="row">
+        {zxc.map((letter) => {
+          const color = usedKeys[letter]
+          return (
+            <div key={letter} className={`item ${color}`}>
+              {letter}
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
